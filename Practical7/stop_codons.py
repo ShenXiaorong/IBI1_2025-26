5ab0814eb9155ed87337aeae2cfc3881ea823f1f
ShenXiaorong <3289605881@qq.com>
2026-05-12 01:45:01 +0800

    Improve stop codon detection logic
    Refactor stop codon detection to find codons after 'ATG'.

--- Practical7/stop_codons.py
+++ Practical7/stop_codons.py
@@ -1,42 +1,57 @@
-input=open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r')
-output=open('stop_genes.fa', 'w')
-header=''
-seq_lines=[]
-import re
-for line in input:
-    line=line.strip()
+input_file = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r')
+output_file = open('stop_genes.fa', 'w')
+
+header = ''
+seq_lines = []
+
+for line in input_file:
+    line = line.strip()
+
     if line.startswith('>'):
         if header:
-           seq=''.join(seq_lines)
-           gene_name = header.split()[0][1:]
-           matches = re.findall(r'(?=(ATG(?:...)*?(TAA|TAG|TGA)))', seq)
-           found_stops = set()
-           for match in matches:
-               stop = match[1]
-               found_stops.add(stop)
-           if found_stops:
+            seq = ''.join(seq_lines)
+            gene_name = header.split()[0][1:]
+
+            start_pos = seq.find('ATG')
+            found_stops = set()
+
+            if start_pos != -1:
+                for i in range(start_pos + 3, len(seq) - 2, 3):
+                    codon = seq[i:i+3]
+                    if codon in ['TAA', 'TAG', 'TGA']:
+                        found_stops.add(codon)
+
+            if found_stops:
                 stop_list = ','.join(sorted(found_stops))
-                output.write(f'>{gene_name};{stop_list}\n')
+                output_file.write(f'>{gene_name};{stop_list}\n')
                 for i in range(0, len(seq), 80):
-                      output.write(seq[i:i+80] + '\n')
-        header=line
-        seq_lines=[]
-        
+                    output_file.write(seq[i:i+80] + '\n')
+
+        header = line
+        seq_lines = []
+
     else:
         seq_lines.append(line)
 
+# process the last gene
 if header:
     seq = ''.join(seq_lines)
     gene_name = header.split()[0][1:]
-    matches = re.findall(r'(?=(ATG(?:...)*?(TAA|TAG|TGA)))', seq)
+
+    start_pos = seq.find('ATG')
     found_stops = set()
-    for match in matches:
-        stop = match[1]
-        found_stops.add(stop)
+
+    if start_pos != -1:
+        for i in range(start_pos + 3, len(seq) - 2, 3):
+            codon = seq[i:i+3]
+            if codon in ['TAA', 'TAG', 'TGA']:
+                found_stops.add(codon)
+
     if found_stops:
         stop_list = ','.join(sorted(found_stops))
-        output.write(f'>{gene_name};{stop_list}\n')
+        output_file.write(f'>{gene_name};{stop_list}\n')
         for i in range(0, len(seq), 80):
-            output.write(seq[i:i+80] + '\n')
-input.close()
-output.close()
+            output_file.write(seq[i:i+80] + '\n')
+
+input_file.close()
+output_file.close()
